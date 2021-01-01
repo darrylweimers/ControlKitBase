7d43b8e91fe58576e8194d6df3cee160e14d0197
Darryl Weimers <darrylweimers@gmail.com>
2021-01-01 16:55:25 -0500

    Initial Commit

---
+++ ControlKitBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ControlKitBase
+//
+//  Created by Darryl Weimers on 2021-01-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
